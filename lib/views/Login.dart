549b7655392794233219d82620a34a827efb43c8
izzudin26 <azidarmor26@gmail.com>
2020-11-11 04:35:22 +0700

    fixing namestate in provider and view

--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todolist/Provider/TodoProvider.dart';
+import 'package:todolist/Provider/UserProvider.dart';
 
 class Login extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return ChangeNotifierProvider(
-        create: (context) => TodoProvider(),
+        create: (context) => UserProvider(),
         child: Scaffold(
           body: Container(
             padding: EdgeInsets.all(10),
@@ -25,13 +27,18 @@ class Login extends StatelessWidget {
                       padding: EdgeInsets.all(20),
                       child: Column(
                         children: [
-                          Consumer<TodoProvider>(
-                              builder: (context, todoprov, _) => TextField(
+                          Consumer<UserProvider>(
+                              builder: (context, userprovider, _){
+                                return TextField(
                                     controller: TextEditingController(
-                                        text: todoprov.fullName),
+                                        text: userprovider.username),
+                                    onChanged: (value){
+                                      userprovider.username = value;
+                                    },
                                     decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Color.fromRGBO(193, 192, 192, 200),
+                                        filled: true,
+                                        fillColor:
+                                            Color.fromRGBO(216, 215, 215, 200),
                                         labelText: "Nama",
                                         prefixIcon: Icon(Icons.people),
                                         border: OutlineInputBorder(
@@ -40,8 +47,10 @@ class Login extends StatelessWidget {
                                             borderSide: BorderSide(
                                               color: Colors.blueAccent,
                                             ))),
-                                  )),
-                          Container(margin: EdgeInsets.only(top: 20),child: btnSubmit())
+                                  );}),
+                          Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child: btnSubmit())
                         ],
                       ),
                     ),
@@ -53,28 +62,43 @@ class Login extends StatelessWidget {
         ));
   }
 
+  void showsnackbar(BuildContext context, String message){
+    final snackbar = SnackBar(content: Text(message), duration: Duration(seconds: 3),);
+    Scaffold.of(context).showSnackBar(snackbar);
+  }
+
   Widget btnSubmit() {
-    return Container(
-      height: 30.0,
-      child: RaisedButton(
-        onPressed: () {},
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-        padding: EdgeInsets.all(0.0),
-        child: Ink(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xff374ABE), Color(0xff64B6FF)],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
+    return Consumer<UserProvider>(
+      builder:(context, userprov, _) => Container(
+        height: 30.0,
+        child: RaisedButton(
+          onPressed: () {
+            final provideruser = Provider.of<UserProvider>(context, listen: false);
+            if(userprov.username == null || userprov.username == ""){
+              showsnackbar(context, "Mohon Mengisi kolom nama");
+            }else{
+              provideruser.setUsername();
+              Navigator.of(context).pushReplacementNamed("MainNavigator");
+            }
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+          padding: EdgeInsets.all(0.0),
+          child: Ink(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(30.0)),
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                "Login",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
               ),
-              borderRadius: BorderRadius.circular(30.0)),
-          child: Container(
-            alignment: Alignment.center,
-            child: Text(
-              "Login",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
             ),
           ),
         ),
